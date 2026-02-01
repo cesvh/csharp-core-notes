b6e1bb26c56b350d5cbbc285e8f33b2e234cd51d
cesvh <cesar.vega.hdez@gmail.com>
2026-01-31 23:38:21 -0600

    feat: Comentarios

---
+++ 001-basico/001-002-comentarios/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Comentarios en C#");
+
+// Comentarios de una sola línea
+
+/*
+Comentarios de múltiples líneas
+*/
+
+Console.WriteLine(); // Comentario al final de una línea de código
+Console.WriteLine("Fin de los comentarios");
