64384cada0841a3cf0c9a0fb5f43dddb3cef0fc7
cesvh <cesar.vega.hdez@gmail.com>
2026-02-02 11:59:13 -0600

    feat: Estados y comportamientos de un objeto

--- 006-clases/001-clases/006-001-001-clases/Program.cs
+++ 006-clases/001-clases/006-001-001-clases/Program.cs
@@ -1,7 +1,29 @@
 ﻿
 Persona personaObj = new Persona();
-//var personaObj2 = new Persona();
+Persona personaObj2 = new Persona();
+//var personaObj0 = new Persona();
+// ????? Revisar la diferencia entre usar var y el tipo de dato explícito
+
+personaObj.nombre = "Yun";
+personaObj.apellidos = "Tianming";
+personaObj.edad = 1991;
+Console.WriteLine(personaObj.obtenerNombreCompleto());
+
+personaObj2.nombre = "R";
+personaObj2.apellidos = "Daneel Olivaw";
+personaObj2.edad = 1991;
+Console.WriteLine(personaObj2.obtenerNombreCompleto());
 
 class Persona
 {
+    // Campos de una clase, estados o atributos de un objeto
+    public string nombre;
+    public string apellidos;
+    public int edad;
+
+    // Métodos de una clase, comportamientos o acciones de un objeto
+    public string obtenerNombreCompleto()
+    {
+        return $"{nombre} {apellidos} {edad}";
+    }
 }
