7d11df3376a8e138c35f97243f5b12d1c485f592
cesvh <cesar.vega.hdez@gmail.com>
2026-02-21 01:26:40 -0600

    fix: c#, corrección de buenas prácticas en c#

--- 006-clases/001-clases/006-001-001-clases/Program.cs
+++ 006-clases/001-clases/006-001-001-clases/Program.cs
@@ -1,28 +1,28 @@
 ﻿
-Persona personaObj = new Persona();
-Persona personaObj2 = new Persona();
+var personaObj = new Persona();
+var personaObj2 = new Persona();
 //var personaObj0 = new Persona();
 // ????? Revisar la diferencia entre usar var y el tipo de dato explícito
 
 personaObj.nombre = "Yun";
 personaObj.apellidos = "Tianming";
 personaObj.edad = 1991;
-Console.WriteLine(personaObj.obtenerNombreCompleto());
+Console.WriteLine(personaObj.ObtenerNombreCompleto());
 
 personaObj2.nombre = "R";
 personaObj2.apellidos = "Daneel Olivaw";
 personaObj2.edad = 1991;
-Console.WriteLine(personaObj2.obtenerNombreCompleto());
+Console.WriteLine(personaObj2.ObtenerNombreCompleto());
 
 class Persona
 {
     // Campos de una clase, estados o atributos de un objeto
-    public string nombre;
-    public string apellidos;
+    public string nombre = "";
+    public string apellidos = "";
     public int edad;
 
     // Métodos de una clase, comportamientos o acciones de un objeto
-    public string obtenerNombreCompleto()
+    public string ObtenerNombreCompleto()
     {
         return $"{nombre} {apellidos} {edad}";
     }
