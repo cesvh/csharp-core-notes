ed30ac686f9ee98f66533f573da7b019f81f6678
cesvh <cesar.vega.hdez@gmail.com>
2026-03-29 12:11:53 -0600

    feat: c#, Arreglos en c#

---
+++ 005-arreglos/001-array/005-001-mc-array/Program.cs
@@ -0,0 +1,7 @@
+﻿string[] nombres = new string[5] {
+    "Liu", "Isaac", "", "", "Loujin"
+};
+
+Console.WriteLine($"nombres[0]: {nombres[0]}");
+Console.WriteLine($"nombres[2]: {nombres[2]}");
+Console.WriteLine($"nombres[4]: {nombres[4]}");
