145b374b08631b8d983fef18b94192b8e0e5b205
cesvh <cesar.vega.hdez@gmail.com>
2026-04-04 14:58:03 -0600

    feat: c#, Propiedades

---
+++ 006-clases/005-propiedades/006-005-mc-propiedades/Program.cs
@@ -0,0 +1,54 @@
+﻿Console.WriteLine("Propiedades");
+
+var usuario = new Usuario("cesar", -12000);
+
+Console.WriteLine($"usuario.Nombre: {usuario.Nombre}");
+Console.WriteLine($"usuario.AnioNacimiento: {usuario.AnioNacimiento}");
+
+usuario.Nombre = "César";
+usuario.AnioNacimiento = 1991;
+
+Console.WriteLine($"usuario.Nombre: {usuario.Nombre}");
+Console.WriteLine($"usuario.AnioNacimiento: {usuario.AnioNacimiento}");
+
+class Usuario {
+    private string _nombre;
+    private int _anioNacimiento;
+
+    public Usuario(string nombre, int anioNacimiento)
+    {
+        _nombre = nombre;
+        _anioNacimiento = anioNacimiento;
+    }
+
+    public string Nombre
+    {
+        get
+        {
+            return _nombre;
+        }
+        set
+        {
+            if (string.IsNullOrEmpty(value)) {
+                value = "Sin nombre";
+            }
+            _nombre = value;
+        }
+    }
+
+    public int AnioNacimiento
+    {
+        get
+        {
+            return _anioNacimiento;
+        }
+        set
+        {
+            if (value < 1901)
+            {
+                value = 1901;
+            }
+            _anioNacimiento = value;
+        }
+    }
+}
