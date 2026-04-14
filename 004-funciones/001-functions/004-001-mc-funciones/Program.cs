baf8b9185b9e3eda8bbc06c186fc95a1bdfa1e5b
cesvh <cesar.vega.hdez@gmail.com>
2026-04-14 00:55:35 -0600

    feat: c#, funciones

---
+++ 004-funciones/001-functions/004-001-mc-funciones/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Funciones");
+Console.WriteLine("");
+
+DatosUsuario();
+
+static void DatosUsuario()
+{
+    Console.WriteLine("César Vega");
+    Console.WriteLine("1991");
+}
+
+
