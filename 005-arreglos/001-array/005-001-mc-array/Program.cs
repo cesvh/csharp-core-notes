574ef2080ddea4006eeab479e153e5afd3aa5425
cesvh <cesar.vega.hdez@gmail.com>
2026-04-17 00:54:23 -0600

    feat: c#, arreglos

--- 005-arreglos/001-array/005-001-mc-array/Program.cs
+++ 005-arreglos/001-array/005-001-mc-array/Program.cs
@@ -1,11 +1,14 @@
-﻿string[] permisos = new string[] {
-    "ver",
-    "consultar",
-    "",
-    "editar",
-    "eliminar"
+﻿Console.WriteLine("Arreglos en C#");
+Console.WriteLine("");
+
+string[] letra = new string[5] {
+    "a",
+    "b",
+    "c",
+    "d",
+    "e"
 };
 
-Console.WriteLine($"permisos[0]:{permisos[0]}");
-Console.WriteLine($"permisos[2]:{permisos[2]}");
-Console.WriteLine($"permisos[4]:{permisos[4]}");
+Console.WriteLine($"letra[0]: {letra[0]}");
+Console.WriteLine($"letra[2]: {letra[2]}");
+Console.WriteLine($"letra[4]: {letra[4]}");
