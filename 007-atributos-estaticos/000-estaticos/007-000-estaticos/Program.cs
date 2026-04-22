f60f02ec22130dab3b56f8646d27fb02e0987750
cesvh <cesar.vega.hdez@gmail.com>
2026-04-21 23:51:05 -0600

    feat: c#, estaticos

--- 007-atributos-estaticos/000-estaticos/007-000-estaticos/Program.cs
+++ 007-atributos-estaticos/000-estaticos/007-000-estaticos/Program.cs
@@ -1,1 +1,27 @@
 ﻿Console.WriteLine("Estaticos");
+Console.WriteLine();
+
+Persona objPersona = new Persona("César", 1991);
+Console.WriteLine($"Cantidad de personas: {Persona.CantidadPersonas}");
+
+Persona objPersona2 = new Persona("Cesar", 1991);
+Console.WriteLine($"Cantidad de personas: {Persona.CantidadPersonas}");
+
+Persona objPersona3 = new Persona("cesar", 1991);
+Console.WriteLine($"Cantidad de personas: {Persona.CantidadPersonas}");
+
+Persona objPersona4 = new Persona("ces", 1991);
+Console.WriteLine($"Cantidad de personas: {Persona.CantidadPersonas}");
+
+class Persona
+{
+    public string Nombre { get; set; }
+    public int Edad { get; set; }
+    public static int CantidadPersonas { get; private set; }
+    public Persona(string nombre, int edad)
+    {
+        Nombre = nombre;
+        Edad = edad;
+        CantidadPersonas++;
+    }
+}
