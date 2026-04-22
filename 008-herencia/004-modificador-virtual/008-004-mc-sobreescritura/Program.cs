07f7ea1599497c0f9963cf2e2de598ef311c76fc
cesvh <cesar.vega.hdez@gmail.com>
2026-04-21 22:27:42 -0600

    feat: c#, Sobre escritura, virtual, override

---
+++ 008-herencia/004-modificador-virtual/008-004-mc-sobreescritura/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Sobre escritura");
+Console.WriteLine();
+
+A objA = new A();
+Console.WriteLine(objA.Saludar());
+
+B objB = new B();
+Console.WriteLine(objB.Saludar());
+
+class A 
+{
+    public virtual string Saludar()
+    {
+        return "Hola desde A";
+    }
+}
+
+class B : A
+{
+    public override string Saludar()
+    {
+        return base.Saludar() + " - Hola desde B";
+    }
+}
