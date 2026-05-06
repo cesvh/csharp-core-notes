b6f45a005cc13a169a4f499dddb87e173ffc657d
cesvh <cesar.vega.hdez@gmail.com>
2026-05-05 21:39:21 -0600

    feat: c#, listas

--- 017-colecciones/001-listas/017-001-001-listas/Program.cs
+++ 017-colecciones/001-listas/017-001-001-listas/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Listas");
+﻿using System.Collections.Generic;
+
+Console.WriteLine("Listas");
+
+List<int> numeros = new List<int>() {
+    2, 9, 9, 7, 9, 2
+};
+
+Console.WriteLine($"Cantidad de elementos en la lista: {numeros.Count}");
+
+foreach (int numero in numeros) {
+    Console.WriteLine($"Número: {numero}");
+};
+
+numeros.Add(4);
+numeros.Add(5);
+
+numeros.Clear();
+Console.WriteLine($"Cantidad de elementos en la lista después de Clear: {numeros.Count}");
