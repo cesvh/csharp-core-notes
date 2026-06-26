6042891290b71af87401f7484717c1d3200d16b9
cesvh <cesar.vega.hdez@gmail.com>
2026-06-26 00:10:49 -0600

    fix: c# using static System.Console; WriteLine

--- 001-basico/001-001-console.writeline/Program.cs
+++ 001-basico/001-001-console.writeline/Program.cs
@@ -1,3 +1,6 @@
 ﻿using static System.Console;
-
+// static: es una directiva que permite utilizar miembros estáticos de una clase sin tener que escribir el nombre de la clase cada vez. En este caso, se está utilizando para poder usar los métodos de la clase Console sin tener que escribir Console. cada vez.
+// Miembros estáticos: son miembros de una clase que pertenecen a la clase en sí misma, en lugar de a instancias individuales de la clase. Se accede a ellos utilizando el nombre de la clase seguido del operador punto (.) y el nombre del miembro estático.
+// System: es un espacio de nombres que contiene clases y tipos fundamentales para el desarrollo de aplicaciones en .NET. Proporciona funcionalidades básicas como entrada/salida, manipulación de cadenas, colecciones, etc.
+// System.Console: es una clase dentro del espacio de nombres System que proporciona métodos y propiedades para interactuar con la consola, como leer y escribir texto, cambiar colores, etc.
 WriteLine("Console.WriteLine");
