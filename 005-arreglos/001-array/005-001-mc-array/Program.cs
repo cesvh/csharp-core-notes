11c6fd5959b4acc7d9757143a3cc9a3403b4806c
cesvh <cesar.vega.hdez@gmail.com>
2026-06-28 18:18:31 -0600

    fix: c# arreglos

--- 005-arreglos/001-array/005-001-mc-array/Program.cs
+++ 005-arreglos/001-array/005-001-mc-array/Program.cs
@@ -1,13 +1,7 @@
 ﻿Console.WriteLine("Arreglos en C#");
 Console.WriteLine("");
 
-string[] letra = new string[5] {
-    "a",
-    "b",
-    "c",
-    "d",
-    "e"
-};
+string[] letra = new string[] { "a", "b", "c", "d", "e" };
 
 Console.WriteLine($"letra[0]: {letra[0]}");
 Console.WriteLine($"letra[2]: {letra[2]}");
