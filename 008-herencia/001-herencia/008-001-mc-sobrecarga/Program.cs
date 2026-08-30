8fc6cdb6f3019d6fbbe77acccb14933f8ba5964d
cesvh <cesar.vega.hdez@gmail.com>
2026-08-30 15:51:53 -0600

    fix: comentarios en sobrecarga

--- 008-herencia/001-herencia/008-001-mc-sobrecarga/Program.cs
+++ 008-herencia/001-herencia/008-001-mc-sobrecarga/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("Sobre carga");
+﻿// La sobrecarga de métodos es una característica de la programación orientada a objetos que permite definir múltiples métodos con el mismo nombre pero con diferentes parámetros. Esto permite que un método pueda realizar diferentes tareas dependiendo de los argumentos que se le pasen. En el ejemplo proporcionado, la clase Math tiene tres métodos Sum con el mismo nombre pero con diferentes tipos y números de parámetros: uno que suma dos enteros, otro que suma dos cadenas de texto convirtiéndolas a enteros, y otro que suma un arreglo de enteros. La sobrecarga de métodos mejora la legibilidad del código y permite una mayor flexibilidad al llamar a los métodos con diferentes tipos de datos o cantidades de argumentos.
+
+Console.WriteLine("Sobre carga");
 Console.WriteLine();
 
 Math math = new Math();
