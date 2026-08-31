c5c0867b3d718128a3f2d9bd9d27923b57403639
cesvh <cesar.vega.hdez@gmail.com>
2026-08-30 22:48:06 -0600

    fix: comentarios de listas

--- 017-colecciones/001-listas/017-001-001-listas/Program.cs
+++ 017-colecciones/001-listas/017-001-001-listas/Program.cs
@@ -2,6 +2,7 @@
 
 Console.WriteLine("Listas");
 
+// List<T> es una clase genérica que representa una lista de objetos que se pueden acceder por índice. Es parte del espacio de nombres System.Collections.Generic.
 List<int> numeros = new List<int>() {
     2, 9, 9, 7, 9, 2
 };
@@ -12,8 +13,8 @@ foreach (int numero in numeros) {
     Console.WriteLine($"Número: {numero}");
 };
 
-numeros.Add(4);
+numeros.Add(4);// Add agrega un elemento al final de la lista
 numeros.Add(5);
 
-numeros.Clear();
+numeros.Clear();// Clear elimina todos los elementos de la lista
 Console.WriteLine($"Cantidad de elementos en la lista después de Clear: {numeros.Count}");
